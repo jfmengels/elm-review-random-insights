6e3b21f5774a0cf90f3db8b38e7b734a006c8c2a
Jeroen Engels <jfm.engels@gmail.com>
2022-11-27 11:21:39 +0100

    Init project

---
+++ tests/FindUntestedModulesTest.elm
@@ -0,0 +1,24 @@
+module FindUntestedModulesTest exposing (all)
+
+import FindUntestedModules exposing (rule)
+import Review.Test
+import Test exposing (Test, describe, test)
+
+
+all : Test
+all =
+    describe "FindUntestedModules"
+        [ test "should report an error when REPLACEME" <|
+            \() ->
+                """module A exposing (..)
+a = 1
+"""
+                    |> Review.Test.run rule
+                    |> Review.Test.expectErrors
+                        [ Review.Test.error
+                            { message = "REPLACEME"
+                            , details = [ "REPLACEME" ]
+                            , under = "REPLACEME"
+                            }
+                        ]
+        ]
